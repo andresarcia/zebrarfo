9837051b12bdeba3a976c6b8a4953ad5b576ef24
spantons <spantons@gmail.com>
2014-09-16 13:22:14 -0430

    [Client] now just render CoordinatesView when the user used the pagination in SinglePlaceView

---
+++ public/javascripts/app/view/CoordinatesView.js
@@ -0,0 +1,20 @@
+var com = com || {};
+com.spantons = com.spantons || {};
+com.spantons.view = com.spantons.view || {};
+
+com.spantons.view.CoordinatesView = Backbone.View.extend({
+
+	template: Handlebars.compile($("#coordiantes-template").html()),
+
+	initialize: function(options){
+	
+	},
+
+	render: function(data){
+		var html = this.template(data);
+    	this.$el.html(html);
+
+		return this;
+	},
+
+});
