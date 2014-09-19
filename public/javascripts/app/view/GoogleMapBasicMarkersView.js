5044f380a518a311b5bcca3cb7b5a731dd91280e
spantons <spantons@gmail.com>
2014-09-19 19:02:47 -0430

    [Client] listen mouse event of makers on googleMapBasicMarkerView

--- public/javascripts/app/view/GoogleMapBasicMarkersView.js
+++ public/javascripts/app/view/GoogleMapBasicMarkersView.js
@@ -5,15 +5,22 @@ com.spantons.view = com.spantons.view || {};
 com.spantons.view.GoogleMapBasicMarkersView = Backbone.View.extend({
 
 	initialize: function(options){
-		
+		this.icon1 = "../../../images/marker_red.png";
+		this.icon2 = "../../../images/marker_green.png";
+		this.mapZoom = 15;
+	},
+
+	markerClick: function(id){
+		console.log(id);
 	},
 
 	render: function(data){
+		var self = this;
 
     	var mapCanvas = document.getElementById('basic-markers-map');
     	var centerCoord = new google.maps.LatLng(data[0].latitude,data[0].longitude);
   		var mapOptions = {
-    		zoom: 15,
+    		zoom: this.mapZoom,
     		center: centerCoord,
     		mapTypeId: google.maps.MapTypeId.ROADMAP
   		};
@@ -21,12 +28,32 @@ com.spantons.view.GoogleMapBasicMarkersView = Backbone.View.extend({
   		var map = new google.maps.Map(mapCanvas, mapOptions);  
 
   		_.each(data, function(coordinate){
+  			var infowindow = new google.maps.InfoWindow({
+  				content: 'Latitude: ' + coordinate.latitude + '<br>Longitude: ' + coordinate.longitude,
+  			});
+  			
   			var latLng = new google.maps.LatLng(coordinate.latitude,coordinate.longitude);
   			var marker = new google.maps.Marker({
 			    position: latLng,
 		      	map: map,
+		      	icon: self.icon1,
+		      	id: coordinate.id,
 		      	title: 'lat:'+coordinate.latitude+' lng:'+coordinate.longitude,
 		  	});
+
+		  	google.maps.event.addListener(marker, 'mouseover', function() {
+		    	marker.setIcon(self.icon2);
+		    	infowindow.open(map, marker);
+			});
+
+			google.maps.event.addListener(marker, 'mouseout', function() {
+		    	marker.setIcon(self.icon1);
+		    	infowindow.close();
+			});
+
+			google.maps.event.addListener(marker, 'click', function(evt) {
+		    	self.markerClick(marker.id);
+			});
   		});
 	}
 
