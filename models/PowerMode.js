dfe4827c86befcb066595d6803426badbb2416c8
Freddy Rondon <spantons@gmail.com>
2015-02-01 22:59:08 -0430

    [Server] new model
    new model PowerMode for save occurrence frequency of power in place

---
+++ models/PowerMode.js
@@ -0,0 +1,27 @@
+// models/PowerFrequency.js
+
+module.exports = function(sequelize, DataTypes) {
+  var PowerMode = sequelize.define('PowerMode', {
+    power: { 
+      type: DataTypes.DECIMAL(18,15),
+      allowNull: false,
+    },
+
+    frequency: { 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    PlaceId: {
+      type: DataTypes.INTEGER,
+      references: "Places",
+      referenceKey: "id",
+      onUpdate: "CASCADE",
+      onDelete: "CASCADE",
+      allowNull: false
+    }
+
+  });
+ 
+  return PowerMode;
+};
