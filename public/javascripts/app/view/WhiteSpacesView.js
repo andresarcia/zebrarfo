6981406fc7895ef5f3c3bb72ff3696a5b952f172
Freddy Rondon <spantons@gmail.com>
2015-05-29 13:27:02 -0430

    [Client] added quality slider WhiteSpaces view

--- public/javascripts/app/view/WhiteSpacesView.js
+++ public/javascripts/app/view/WhiteSpacesView.js
@@ -4,7 +4,14 @@ app.view = app.view || {};
 app.view.WhiteSpacesView = Backbone.View.extend({
 
 	events: {
-		
+		'change .quality-slider':'changeQuality',
+	},
+
+	reset: function(){
+		this.settings = {};
+		this.settings.quality = {};
+		this.settings.quality.crr = 5;
+		this.settings.quality.max = 10;
 	},
 
 	initialize: function(options){
@@ -14,11 +21,60 @@ app.view.WhiteSpacesView = Backbone.View.extend({
 
 		this.data = window.place.attributes;
 		this.data3D = null;
+		this.reset();
 
 		$(window).on('resize', { reference: this }, this.renderComponents);
 	},
 
+	renderComponents: function(evt){
+		var self;
+		if(evt)
+			self = evt.data.reference;
+		else
+			self = this;
+
+		this.renderSettings();
+
+		if(window.settings.googleVisualizationApi)
+			self.renderGraph(this.coordinates);
+		else {
+			Backbone.pubSub.off('event-loaded-google-visualization-api');
+			Backbone.pubSub.on('event-loaded-google-visualization-api', function(){
+				self.renderGraph(self.coordinates);
+			});
+		}
+	},
+
+	renderSettings: function(){
+		this.qualitySlider = this.$el.find('.quality-slider').noUiSlider({
+			start: this.settings.quality.crr,
+			step: 1,
+			format: wNumb({
+				decimals: 0
+			}),
+			range: {
+				'min': 1,
+				'max': 9
+			}
+		});
+
+		this.$el.find('.quality-slider')
+		.Link('lower')
+		.to('-inline-<div class="slider_tooltip bottom"></div>', function(value){
+			$(this).html('<strong>' + value + '</strong>');
+			$(this).css('width', '50px');
+			$(this).css('left', '-10px');
+		});
+	},
+
+	changeQuality: function(){
+		this.settings.quality.crr = this.settings.quality.max - this.qualitySlider.val();
+		this.data3D = undefined;
+		this.renderGraph();
+	},
+
 	calculateData: function(){
+		this.data3D = {};
 		this.data3D = new google.visualization.DataTable();
 		this.data3D.addColumn('number', 'Power Threshold');
 		this.data3D.addColumn('number', 'Frequencies (MHz)');
@@ -53,8 +109,7 @@ app.view.WhiteSpacesView = Backbone.View.extend({
 			data = dataFiltered;
 		} 
 
-		var presition = 3;
-		for (var i = this.data.powerMax - 1; i >= this.data.powerMin; i -= presition) {
+		for (var i = this.data.powerMax - 1; i >= this.data.powerMin; i -= this.settings.quality.crr) {
 			_.each(data, function(itemSameFrequency){
 				var passed = 0;
 				_.each(itemSameFrequency, function(item){
@@ -68,26 +123,12 @@ app.view.WhiteSpacesView = Backbone.View.extend({
 		}
 	},
 
-	renderComponents: function(evt){
-		var self;
-		if(evt)
-			self = evt.data.reference;
-		else
-			self = this;
-
-		if(window.settings.googleVisualizationApi)
-			self.renderGraph(this.coordinates);
-		else {
-			Backbone.pubSub.off('event-loaded-google-visualization-api');
-			Backbone.pubSub.on('event-loaded-google-visualization-api', function(){
-				self.renderGraph(self.coordinates);
-			});
-		}
-	},
-
 	renderGraph: function(){
 		this.waitingView.show();
-		if(!this.data3D) this.calculateData();
+		if(!this.data3D) {
+			this.disableSettings();
+			this.calculateData();
+		}
 
 		var options = {
 			width:  "100%",
@@ -109,13 +150,21 @@ app.view.WhiteSpacesView = Backbone.View.extend({
 		setTimeout(function(){
 			graph.draw(self.data3D, options);
 			self.waitingView.hide();
+			self.$el.find('.settings').removeClass('disable-container');
 		}, 200);
+	},
+
+	disableSettings: function(){
+		if(!this.$el.find('.settings').hasClass("disable-container"))
+			this.$el.find('.settings').addClass('disable-container');
 	},
 
 	render: function(){
 		var template = Zebra.tmpl.white_spaces;
 		var html = template();
 		this.$el.html(html);
+		
+		this.disableSettings();
 
 		return this;
 	},
