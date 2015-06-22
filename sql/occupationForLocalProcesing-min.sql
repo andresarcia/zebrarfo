ac8614ba6a4a81165a4534f61922a18d2295d879
Freddy Rondon <spantons@gmail.com>
2015-06-22 11:39:37 -0430

    ref: remove unnecessary folders

--- sql/occupationForLocalProcesing-min.sql
+++
@@ -1,1 +0,0 @@
-select frequency,power from (select Coordinates.id from (select id from Places where id = 1 ) as aux, Coordinates where Coordinates.PlaceId = aux.id) as aux, PowerFrequencies where aux.id = PowerFrequencies.CoordinateId order by frequency
