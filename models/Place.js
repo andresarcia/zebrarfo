c471593983555d3916aa28a541a136cd94a6cba0
Freddy Rondon <spantons@gmail.com>
2015-07-13 22:29:43 -0430

    ref[Server]: refactor the place model to save like string json the power object, the distance object and the frequency object from the builder

--- models/Place.js
+++ models/Place.js
@@ -6,72 +6,28 @@ module.exports = function(sequelize, DataTypes) {
         type: DataTypes.STRING,
         allowNull: false,
     },
+
     numberCoordinates: {
       type: DataTypes.INTEGER,
       allowNull: true,
       defaultValue: 0,
     },
-    powerMin: {
-      type: DataTypes.DECIMAL(10,6),
-      allowNull: true,
-    },
-    powerMax : {
-      type: DataTypes.DECIMAL(10,6),
-      allowNull: true,
-    },
-    powerAvg : {
-      type: DataTypes.DECIMAL(10,6),
-      allowNull: true,
-    },
-    sdPowerAvg : {
-      type: DataTypes.DECIMAL(10,6),
-      allowNull: true,
-    },
-    avgPowerSD : {
-      type: DataTypes.DECIMAL(10,6),
-      allowNull: true,
-    },
-    numberPowerFrequency: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      defaultValue: 0,
-    },
-    frequencyMin: {
-      type: DataTypes.DECIMAL(12,3),
-      allowNull: true,
-    },
-    frequencyMax: {
-      type: DataTypes.DECIMAL(12,3),
-      allowNull: true,
-    },
-    frequenciesBands: {
+
+    power: {
       type: DataTypes.TEXT,
       allowNull: true,
     },
-    frequenciesChannelWidth: {
+
+    frequencies: {
       type: DataTypes.TEXT,
       allowNull: true,
     },
-    totalDistance: {
-      type: DataTypes.DECIMAL(12,3),
-      allowNull: true,
-    },
-    distanceAvg: {
-      type: DataTypes.DECIMAL(12,7),
-      allowNull: true,
-    },
-    distanceMin: {
-      type: DataTypes.DECIMAL(12,7),
-      allowNull: true,
-    },
-    distanceMax: {
-      type: DataTypes.DECIMAL(12,7),
-      allowNull: true,
-    },
-    distanceSD: {
+
+    distance: {
       type: DataTypes.TEXT,
       allowNull: true,
     },
+
     visible : {
       type:DataTypes.BOOLEAN,
       defaultValue: 1
